34c658816b52792a99a79bde7989fde909f7560f
Islam Wael <135991348+islamwael86@users.noreply.github.com>
2023-12-10 16:31:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -27,32 +27,32 @@ const myJsObject = JSON.parse(data);
 console.log(myJsObject);
 
 let Reaction1 = document.querySelector(".svg-one");
-let Reaction2 = document.querySelector(".reaction .category-one");
-let Reaction3 = document.querySelector(".reaction .black");
+let Reaction2 = document.querySelector("#reaction .category-one");
+let Reaction3 = document.querySelector("#reaction .black");
 
 Reaction1.setAttribute("src",  myJsObject[0]["icon"]);
 Reaction2.innerHTML = myJsObject[0]["category"];
 Reaction3.textContent = myJsObject[0]["score"];
 
 let Memory1 = document.querySelector(".svg-two");
-let Memory2 = document.querySelector(".memory .category-two");
-let Memory3 = document.querySelector(".memory .black");
+let Memory2 = document.querySelector("#memory .category-two");
+let Memory3 = document.querySelector("#memory .black");
 
 Memory1.setAttribute("src",  myJsObject[1]["icon"]);
 Memory2.innerHTML = myJsObject[1]["category"];
 Memory3.textContent = myJsObject[1]["score"];
 
 let Verbal1 = document.querySelector(".svg-three");
-let Verbal2 = document.querySelector(".verbal .category-three");
-let Verbal3 = document.querySelector(".verbal .black");
+let Verbal2 = document.querySelector("#verbal .category-three");
+let Verbal3 = document.querySelector("#verbal .black");
 
 Verbal1.setAttribute("src",  myJsObject[2]["icon"]);
 Verbal2.innerHTML = myJsObject[2]["category"];
 Verbal3.textContent = myJsObject[2]["score"];
 
 let Visual1 = document.querySelector(".svg-four");
-let Visual2 = document.querySelector(".visual .category-four");
-let Visual3 = document.querySelector(".visual .black");
+let Visual2 = document.querySelector("#visual .category-four");
+let Visual3 = document.querySelector("#visual .black");
 
 Visual1.setAttribute("src",  myJsObject[3]["icon"]);
 Visual2.innerHTML = myJsObject[3]["category"];
